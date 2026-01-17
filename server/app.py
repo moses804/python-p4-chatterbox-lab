549e832f14ccef6a88357942993d13fbc9f32725
Moses Maina <mainacalton57@gmail.com>
2026-01-17 03:27:40 +0300

    lab complete

--- server/app.py
+++ server/app.py
@@ -14,13 +14,41 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        # GET /messages: return all messages ordered by created_at ascending
+        messages = Message.query.order_by(Message.created_at.asc()).all()
+        return jsonify([message.to_dict() for message in messages])
+    
+    elif request.method == 'POST':
+        # POST /messages: create a new message
+        data = request.get_json()
+        new_message = Message(
+            body=data.get('body'),
+            username=data.get('username')
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        return jsonify(new_message.to_dict()), 201
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.get_or_404(id)
+    
+    if request.method == 'PATCH':
+        # PATCH /messages/<id>: update message body
+        data = request.get_json()
+        if 'body' in data:
+            message.body = data['body']
+        db.session.commit()
+        return jsonify(message.to_dict())
+    
+    elif request.method == 'DELETE':
+        # DELETE /messages/<id>: delete message
+        db.session.delete(message)
+        db.session.commit()
+        return '', 204
 
 if __name__ == '__main__':
     app.run(port=5555)
